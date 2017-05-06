6786d55a45987cecf78ba602850b59211ff68c08
Anoxic <guanrunjie@gmail.com>
2017-05-05 17:31:46 -0700

    Markers are now displayed on the map

--- journal-react/src/BulbMap.js
+++ journal-react/src/BulbMap.js
@@ -12,67 +12,57 @@ import {
 } from "react-google-maps";
 import MarkerClusterer from 'react-google-maps/lib/addons/MarkerClusterer';
 
-import cluster0 from "./imgs/map-cluster-0.svg";
-import cluster1 from "./imgs/map-cluster-1.svg";
-import cluster2 from "./imgs/map-cluster-2.svg";
-import cluster3 from "./imgs/map-cluster-3.svg";
-import cluster4 from "./imgs/map-cluster-4.svg";
-import cluster5 from "./imgs/map-cluster-5.svg";
-import cluster6 from "./imgs/map-cluster-6.svg";
-import cluster7 from "./imgs/map-cluster-7.svg";
-import cluster8 from "./imgs/map-cluster-8.svg";
+var getGoogleClusterInlineSvg = function(color) {
+  let encoded = window.btoa(
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-100 -100 200 200"><defs><g id="a" transform="rotate(45)"><path d="M0 47A47 47 0 0 0 47 0L62 0A62 62 0 0 1 0 62Z" fill-opacity="0.7"/></g></defs><g fill="' + color + '"><circle r="42"/><use xlink:href="#a"/><g transform="rotate(120)"><use xlink:href="#a"/></g><g transform="rotate(240)"><use xlink:href="#a"/></g></g></svg>');
 
+  return ('data:image/svg+xml;base64,' + encoded);
+};
 
+const mapMarkerIcon = `data:image/svg+xml;base64,${window.btoa(
+    '<svg width="10" height="10" xmlns="http://www.w3.org/2000/svg"><g><ellipse ry="5" rx="5" cy="5" cx="5" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#212121"/></g></svg>')}`;
 
-const clusterSize = 25;
 const clusterStyles = [
   {
-    textColor: 'black',
-    url      : {cluster0}.cluster0,
-    height   : clusterSize,
-    width    : clusterSize
-  }, {
-    textColor: 'black',
-    url      : {cluster1}.cluster1,
-    height   : clusterSize,
-    width    : clusterSize
-  }, {
-    textColor: 'black',
-    url      : {cluster2}.cluster2,
-    height   : clusterSize,
-    width    : clusterSize
-  }, {
-    textColor: 'black',
-    url      : {cluster3}.cluster3,
-    height   : clusterSize,
-    width    : clusterSize
-  }, {
-    textColor: 'black',
-    url      : {cluster4}.cluster4,
-    height   : clusterSize,
-    width    : clusterSize
-  }, {
-    textColor: 'white',
-    url      : {cluster5}.cluster5,
-    height   : clusterSize,
-    width    : clusterSize
-  }, {
-    textColor: 'white',
-    url      : {cluster6}.cluster6,
-    height   : clusterSize,
-    width    : clusterSize
-  }, {
-    textColor: 'white',
-    url      : {cluster7}.cluster7,
-    height   : clusterSize,
-    width    : clusterSize
-  }, {
-    textColor: 'white',
-    url      : {cluster8}.cluster8,
-    height   : clusterSize,
-    width    : clusterSize
+    width     : 40,
+    height    : 40,
+    url       : getGoogleClusterInlineSvg('#80D8FF'),
+    textColor : "black",
+    textSize  : 12,
+    fontFamily: `"Lato", "Noto Sans CJK SC", "Noto Sans SC", sans-serif`,
   },
-
+  {
+    width     : 60,
+    height    : 60,
+    url       : getGoogleClusterInlineSvg('#40C4FF'),
+    textColor : 'black',
+    textSize  : 14,
+    fontFamily: `"Lato", "Noto Sans CJK SC", "Noto Sans SC", sans-serif`,
+  },
+  {
+    width     : 80,
+    height    : 80,
+    url       : getGoogleClusterInlineSvg('#00B0FF'),
+    textColor : 'black',
+    textSize  : 16,
+    fontFamily: `"Lato", "Noto Sans CJK SC", "Noto Sans SC", sans-serif`,
+  },
+  {
+    width     : 100,
+    height    : 100,
+    url       : getGoogleClusterInlineSvg('#0091EA'),
+    textColor : 'black',
+    textSize  : 18,
+    fontFamily: `"Lato", "Noto Sans CJK SC", "Noto Sans SC", sans-serif`,
+  },
+  {
+    width     : 130,
+    height    : 130,
+    url       : getGoogleClusterInlineSvg('#01579B'),
+    textColor : 'white',
+    textSize  : 18,
+    fontFamily: `"Lato", "Noto Sans CJK SC", "Noto Sans SC", sans-serif`,
+  }
 ];
 
 const BulbGoogleMap = withGoogleMap(props => (
@@ -93,6 +83,7 @@ const BulbGoogleMap = withGoogleMap(props => (
                 <Marker
                     key={bulb.time.created}
                     position={{lat:parseFloat(bulb.place.latitude,10),lng:parseFloat(bulb.place.longitude,10)}}
+                    icon={mapMarkerIcon}
                 />
             );
           } else {
@@ -149,8 +140,6 @@ export default class BulbMap extends Component {
   }
 
   render() {
-    console.log({cluster1});
-
     return (
         <div className="BulbMap" style={{height: `100%`}}>
           <BulbGoogleMap
