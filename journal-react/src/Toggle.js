1ba590b7d44a732bf60f50a229ca64d4b195ebd6
Anoxic <guanrunjie@gmail.com>
2017-06-08 15:06:30 -0700

    Fixed the styling of toggles

--- journal-react/src/Toggle.js
+++ journal-react/src/Toggle.js
@@ -13,22 +13,33 @@ export default class Toggle extends Component {
   };
 
   render() {
-    let {className, isHidden, isChangingOnHover, isChanging, onClick, height, firstIcon, secondIcon} = this.props;
+    let {className, isHidden, isChangingOnHover, isChanging, onClick, firstIcon, secondIcon} = this.props;
 
     let disabled = {};
     if (this.props.disabled) {
       disabled.disabled = "disabled";
     }
 
-    height = height || "50px";
-
     return (
         <a className={`toggle ${this.props.loading ? "loading" : ""} ${className || ""} ${isHidden ? "hidden" : ""} ${isChangingOnHover ? "change-hover" : ""} ${isChanging ? "show-second" : ""} `}
            onClick={onClick}
-           style={{height: height}}
             {...disabled}
         >
           <Ink/>
+          <div
+              className={`flex-center first icon-wrapper ${!this.props.loading ? "" : "transparent"}`}>
+            <i className="material-icons">{firstIcon}</i>
+          </div>
+          <div
+              className={`flex-center second icon-wrapper ${!this.props.loading ? "" : "transparent"}`}>
+            <i className="material-icons">{secondIcon}</i>
+          </div>
+          { this.props.tooltip ? (
+              <div className="tooltip flex-center">
+                <span className="tooltip-text">
+                  {this.props.tooltip}
+                </span>
+              </div>) : null }
           { this.props.tooltip ? (
               <div className="tooltip flex-center">
                 <span className="tooltip-text">
@@ -37,12 +48,8 @@ export default class Toggle extends Component {
               </div>) : null }
           <div
               className={`flex-center icon-wrapper loading-icon ${this.props.loading ? "" : "transparent"}`}>
-            <i className="material-icons">
-              remove
-            </i>
+            <i className="material-icons">remove</i>
           </div>
-          <i className="flex-center first material-icons">{firstIcon}</i>
-          <i className="flex-center second material-icons">{secondIcon}</i>
         </a>
     );
   }
