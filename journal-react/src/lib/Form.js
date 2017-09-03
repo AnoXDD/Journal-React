10f85cec6d935ea6f3637e72d293e329e088d7c4
Anoxic <guanrunjie@gmail.com>
2017-09-03 11:48:54 -0700

    Added Form.js

--- journal-react/src/lib/Form.js
+++ journal-react/src/lib/Form.js
@@ -5,15 +5,17 @@
  */
 
 import React, {Component} from 'react';
+import Button from "./Button";
+import Toggle from "./Toggle";
 
 class FormContent extends Component {
   render() {
     return (
-        <div className={`form-content ${this.props.className || ""}`}>
-          <div
-              className={`description ${this.props.subTitle ? "sub-title" : ""}`}>{this.props.title}</div>
-          <div className="btns">{this.props.children}</div>
-        </div>
+      <div className={`form-content ${this.props.className || ""}`}>
+        <div
+          className={`description ${this.props.subTitle ? "sub-title" : ""}`}>{this.props.title}</div>
+        <div className="btns">{this.props.children}</div>
+      </div>
     );
   }
 }
@@ -67,20 +69,87 @@ class DigitInput extends Component {
 
   render() {
     return (
-        <input type="text" className={this.props.className || ""}
-               name={this.props.name}
-               value={this.props.value}
-               onChange={this.handleChange}/>
+      <input type="text" className={this.props.className || ""}
+             name={this.props.name}
+             value={this.props.value}
+             onChange={this.handleChange}/>
     )
   }
 }
 
+module.exports = {
+  type: {
+    NONE       : 1,
+    PLAIN_TEXT : 2,
+    TOGGLE     : 3,
+    BUTTON     : 4,
+    INPUT      : 5,
+    DIGIT_INPUT: 6,
+  }
+};
+
 export default class Form extends Component {
+
   render() {
     return (
-        <div className={`form ${this.props.className || ""}`}>
+      <div className={`form ${this.props.className || ""}`}>
+        {this.props.data.map(form =>
+          <div className="form shadow">
+            <div className="form-title">{form.content}</div>
+            {!form.row.map ? form.row : form.row.map(row =>
+              <div className="form-row">
+                <div className="title-dark flex-center">{row.title}</div>
+                <div className="form-contents">
+                  {!row.content.map ? row.content : row.content.map(content =>
+                    <FormContent
+                      className={content.className}
+                      title={content.title} {content.subTitle}>
+                      {!content.elements.map ? content.elements
+                        : content.elements.map(
+                          elem => {
+                            switch (elem.type) {
+                              case Form.type.NONE:
+                                return (<span></span>);
 
-        </div>
+                              case Form.type.PLAIN_TEXT:
+                                return (
+                                  <p className="plain-text">elem.props</p>
+                                );
+
+                              case Form.type.TOGGLE:
+                                return (<Toggle {...elem.props}/>);
+
+                              case Form.type.BUTTON:
+                                return (<Button {...elem.props}/>);
+
+                              case Form.type.INPUT:
+                                ({label, props} = elem.props);
+                                return ([
+                                  <p className="input-label">{label}</p>,
+                                  <div className="flex-center">
+                                    <input {...props}/>
+                                  </div>
+                                ]);
+
+                              case Form.type.DIGIT_INPUT:
+                                ({label, props} = elem.props);
+                                return ([
+                                  <p className="input-label">{label}</p>,
+                                  <div className="flex-center">
+                                    <DigitInput {...props}/>
+                                  </div>
+                                ]);
+                            }
+                          }
+                        )}
+                    </FormContent>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
     );
   }
 }
